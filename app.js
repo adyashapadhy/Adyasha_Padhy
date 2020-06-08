4d180536f87b8c096f46832c0f3ff267f4839ecc
ADYASHA <adyasha@ADYASHAs-MacBook-Air.local>
2020-06-08 19:53:24 +0530

    height changes

--- app.js
+++ app.js
@@ -24,3 +24,16 @@ const navSlide = ()=>{
 }
 
 navSlide();
+
+/*
+const login = document.getElementById('login');
+const loginMenu = document.getElementById('loginMenu');
+
+login.addEventListener('click', () => {
+  if(loginMenu.style.display === 'none'){
+    loginMenu.style.display = 'inline';
+  } else {
+    loginMenu.style.display = 'none';
+  }
+});
+*/
