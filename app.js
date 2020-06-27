5f2b73f29d35dca32bc9b5521f149e8f79a0c85a
ADYASHA <adyasha@ADYASHAs-MacBook-Air.local>
2020-06-27 17:35:02 +0530

    Card design addition

--- app.js
+++ app.js
@@ -1,39 +1,8 @@
-const navSlide = ()=>{
-    const burger=document.querySelector('.burger');
-    const nav = document.querySelector('.nav-bar');
-    const navLinks=document.querySelectorAll('.nav-link')
 
-   
-    burger.addEventListener('click', () => {
-    //Toggle Nav
-    nav.classList.toggle('nav-active');
-    
 
-    //animate Links
-    navLinks.forEach((link,index) => {
-        if(link.style.animation){
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navLink = document.getElementsByClassName('.nav-link')[0]
 
-            link.style.animation = '';
-        } else {
-            link.style.animation = link.style.animation =`navLinkFade 0.5s ease forwards ${index / 5 + 1}s`;
-        }
-      });
-      //burger animation
-      burger.classList.toggle('toggle');
-    });
-}
-
-navSlide();
-
-/*
-const login = document.getElementById('login');
-const loginMenu = document.getElementById('loginMenu');
-
-login.addEventListener('click', () => {
-  if(loginMenu.style.display === 'none'){
-    loginMenu.style.display = 'inline';
-  } else {
-    loginMenu.style.display = 'none';
-  }
-});
-*/
+toggleButton.addEventListener('click', () => {
+  navLink.classList.toggle('active')
+})
