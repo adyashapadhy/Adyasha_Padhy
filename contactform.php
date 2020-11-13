0738a4acc8e552931c811fcd4aea6391e651798f
adyashapadhy <adya0812@gmail.com>
2020-11-13 17:28:50 +0530

    New changes

--- contactform.php
+++ contactform.php
@@ -8,19 +8,19 @@
 
    
     //database connection 
-    $conn = new mysqli('localhost','root','','test');
+    $conn = new mysqli('localhost','root','','test'); //opens new connection to the mysql server
 	if($conn->connect_error){
 		echo "$conn->connect_error";
-		die("Connection Failed : ". $conn->connect_error);
+		die("Connection Failed : ". $conn->connect_error);  //displays error if the connection fails
     } 
     else {
-		$stmt = $conn->prepare("INSERT INTO contact (name, email, message) values(?, ?, ?)");
-		$stmt->bind_param("sss", $name, $email, $message);
-		$execval = $stmt->execute();
+		$stmt = $conn->prepare("INSERT INTO contact (name, email, message) values(?, ?, ?)"); //prepare a sql statement for execution
+		$stmt->bind_param("sss", $name, $email, $message); //Binds variables to a prepared statement as parameters
+		$execval = $stmt->execute();  //executes the prepared query 
 		echo $execval;
 		echo "Message sent succesfully";
 		$stmt->close();
-		$conn->close();
+		$conn->close(); //closes previously opened database connection
     }
     }
 ?>
